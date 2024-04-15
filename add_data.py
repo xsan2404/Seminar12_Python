4584b676bcd0f541586f1df0b547f99b5622e69c
xsan2404 <153509548+xsan2404@users.noreply.github.com>
2024-04-15 09:50:55 +0400

    Add files via upload

---
+++ add_data.py
@@ -0,0 +1,15 @@
+from return_data_file import data_file
+
+
+def add_row():
+    name = input("Введите свое имя: ")
+    surname = input("Введите свою фамилию: ")
+    birthdate = input("Введите дату рождения: ")
+    town = input("Введите город: ")
+    phone = input("Введите телефон: ")
+    data, nf = data_file()
+    now_number_row = len(data) + 1
+    with open(f'db/data_{nf}.txt', 'a', encoding='utf-8') as file:
+        file.write(f'{now_number_row};{name};'
+                   f'{surname};{birthdate};{town};{phone}\n')
+    print("Данные успешно записаны!")
